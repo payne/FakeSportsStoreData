761314385cf90d330f7caae375ccfda26d5a0dbb
Matt Payne <Payne@MattPayne.org>
2019-07-20 21:12:47 -0500

    Changed from people to products

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ function generateUsers() {
 
 				  for (let id=1; id <= 100; id++) {
 
-									    let firstName = faker.name.firstName();
-									    let lastName = faker.name.lastName();
-									    let email = faker.internet.email();
+									const productName = faker.commerce.productName();
+									const department = faker.commerce.department();
+									const price = faker.commerce.price();
 
 									    users.push({
-															        "id": id,
-															        "first_name": firstName,
-															        "last_name": lastName,
-															        "email": email
+															"id": id,
+															"productName": productName,
+															"department": department,
+															"price": price
 															    });
 									  }
 
@@ -28,3 +28,47 @@ function generateUsers() {
 let dataObj = generateUsers();
 
 fs.writeFileSync('data.json', JSON.stringify(dataObj, null, '\t'));
+
+/*
+Usage: faker-cli [option]
+
+A cli wrapper for fakerjs
+
+Options:
+  -V, --version            output the version number
+  -a, --address  [option]  Street address
+  -c, --company  [option]  Company info
+  -d, --date     [option]  Date options
+  -f  --finance  [option]  Finance field
+  -i  --internet [option]  Internet goodies
+  -l  --lorem    [option]  Lorem ipsum goodness
+  -n  --names    [option]  Person name(s)
+  -s  --system   [option]  System Info
+  -C  --commerce [option]  Commerce related info 
+  -p  --phone    [option]  Phone number options
+  -r  --random   [option]  Randomness
+  -L, --locale   [option]  Set locale, defaults to en (default: "en")
+  -x, --hacker   [option]  Hackers stuff
+  -H, --helpers  [option]  Detailed contextual data
+  -I, --image    [option]  Image data
+  -D, --database [option]  Database stuff
+  --locales                List available locales
+  -h, --help               output usage information
+  faker-cli <cmd> help    List options available for <cmd>
+
+  Examples
+
+	$ faker-cli --helpers userCard
+	$ faker-cli --random uuid
+	$ faker-cli --locale de -H userCard
+
+     Available options for commerce:
+	color
+	department
+	productName
+	price
+	productAdjective
+	productMaterial
+	product
+
+*/
